dae46f5812a6e8a98c8a7641d1b1973700991ee7
spyretos09 <122184211+spyretos09@users.noreply.github.com>
2024-02-26 17:20:15 +0200

    final working

---
+++ test.py
@@ -0,0 +1,18 @@
+import requests
+
+
+url = input("Enter the URL: ")      #Δώσε το url
+r = requests.get(url)
+
+print(f"Headers are: ,{r.headers} \n\n") #headers
+
+print("a) Server: ",r.headers.get('Server'))
+
+cookies = r.cookies
+if cookies:                         #cookies κωδικας
+    print("b) Cookies: yes ")
+    for cookie in  r.cookies:
+            print("Cookie name: ",cookie.name)
+else:
+    print("Not using Cookies !!!")
+
